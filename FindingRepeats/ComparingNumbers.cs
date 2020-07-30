510b7b0eb5cf09493b73c6aeb97ac92c75fa81b1
Barbara Gska <pacuszka.barbara@gmail.com>
2020-07-30 20:18:23 +0200

    Adding project files

---
+++ FindingRepeats/ComparingNumbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindingRepeats
+{
+    public static class ComparingNumbers
+    {
+        public static bool CompareNumbers(string number1, string number2)
+        {
+            char repeatedNumber = ' ';
+            
+            for (int i = 0; i < (number1.Length - 2); i++)
+            {
+                if (number1[i].Equals(number1[i +1]) && number1[i].Equals(number1[i + 2]))
+                {
+                    repeatedNumber = number1[i];
+                    break;
+                }
+            }
+
+            for (int i = 0; i < (number2.Length - 1); i++)
+            {
+                if (number2[i].Equals(number2[i + 1]) && number2[i] == repeatedNumber)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ FindingRepeats/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindingRepeats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj pierwszą liczbę: ");
+            var number1 = Console.ReadLine();
+            Console.Write("Podaj drugą liczbę: ");
+            var number2 = Console.ReadLine();
+
+            if (int.TryParse(number1, out int result1) && int.TryParse(number2, out int result2))
+            {
+                Console.Write("Podane liczby spełniają założenia: ");
+
+                if (ComparingNumbers.CompareNumbers(number1, number2))
+                {
+                    Console.WriteLine("Prawda");
+                }
+                else
+                    Console.WriteLine("Fałsz");
+            }
+            else
+            {
+                Console.WriteLine("Wprowadzono nieprawidłowe dane");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
